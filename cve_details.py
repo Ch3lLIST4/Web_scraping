bdee0a7a6ae52ccda6a3e089af762a32a5ccb953
Ch3lLIST4 <herohoang74@gmail.com>
2020-11-06 20:36:53 +0700

    Update cve_details.py

--- cve_details.py
+++ cve_details.py
@@ -5,6 +5,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 R = '\033[31m' # red
 G = '\033[32m' # green
 C = '\033[36m' # cyan
@@ -42,15 +43,17 @@ def cve_details():
             str = str.replace('\t', '\n')
             while '\n\n' in str:
                 str = str.replace('\n\n', '\n')
-            str = 'CVE Description : ' + str
-            str = str + '\nCVE Details Url : ' + target
+            str = R + 'CVE Description : ' + W + str 
+            str = str + '\n' + R + 'CVE Details Url : ' + W + target
+            str = str.replace('Last Update Date :', R + 'Last Update Date :' + W)
+            str = str.replace('Publish Date :', R + 'Publish Date :' + W)
             print(str)
 
         else:
             print(R + '[-]' + C + ' Response Status Code is not 200' + W)
     except IndexError as e:
         print("""
-Usage: cve_details.py address
+Usage: cve_details.py cve-code
         """)
     except Exception as e:
         print(R + '[-] Exception : ' + C + str(e) + W)
